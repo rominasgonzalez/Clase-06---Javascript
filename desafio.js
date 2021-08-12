4b5569946ce7372ff630905dde2a1038d7981835
Romina Solange Gonzalez <romy.s.gonzalez@gamil.com>
2021-08-11 23:59:59 -0300

    clase

---
+++ desafio.js
@@ -0,0 +1,11 @@
+ const listaHamburguesa = [];
+ let   cantidad     = 2;
+
+
+ do{
+   let entrada = prompt("Ingresar tu combo");
+   listaHamburguesa.push(entrada.toUpperCase());
+   console.log(listaHamburguesa.length);
+ }while(listaHamburguesa.length != cantidad) {
+   alert("Productos: " + listaHamburguesa);
+ }
